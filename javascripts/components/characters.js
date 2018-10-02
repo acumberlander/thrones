10c28a3b94c3c194ae346ab2585da4d6d5fcc9d8
Austin Cumberlander <austincumberlander@gmail.com>
2018-10-02 15:13:20 -0500

    finished product

--- javascripts/components/characters.js
+++ javascripts/components/characters.js
@@ -3,17 +3,33 @@ import {printToDom} from "../helpers/util.js";
 console.log("characters.js working");
 
 const characters = [
-    {name: "John Snow", house: "Stark", imageUrl: "https://vignette.wikia.nocookie.net/gameofthrones/images/a/a5/Profile-JonSnow-707.png/revision/latest?cb=20170828030553"},
-    {name: "Daenerys Targaryen", house: "Targaryen", imageUrl: "https://vignette.wikia.nocookie.net/gameofthrones/images/a/a5/Profile-JonSnow-707.png/revision/latest?cb=20170828030553"},
-    {name: "Cersei Lannister", house: "Lannister", imageUrl: "https://vignette.wikia.nocookie.net/gameofthrones/images/a/a5/Profile-JonSnow-707.png/revision/latest?cb=20170828030553"},
-    {name: "Tyrion Lannister", house: "Lannister", imageUrl: "https://vignette.wikia.nocookie.net/gameofthrones/images/a/a5/Profile-JonSnow-707.png/revision/latest?cb=20170828030553"}
+    {id: "character1", name: "John Snow", house: "Stark", imageUrl: "https://vignette.wikia.nocookie.net/gameofthrones/images/a/a5/Profile-JonSnow-707.png/revision/latest?cb=20170828030553"},
+    {id: "character2", name: "a", house: "Targaryen", imageUrl: "https://vignette.wikia.nocookie.net/gameofthrones/images/a/a5/Profile-JonSnow-707.png/revision/latest?cb=20170828030553"},
+    {id: "character3", name: "b", house: "Lannister", imageUrl: "https://vignette.wikia.nocookie.net/gameofthrones/images/a/a5/Profile-JonSnow-707.png/revision/latest?cb=20170828030553"},
+    {id: "character4", name: "c", house: "Lannister", imageUrl: "https://vignette.wikia.nocookie.net/gameofthrones/images/a/a5/Profile-JonSnow-707.png/revision/latest?cb=20170828030553"}
 ];
 console.log(characters);
+
+const characterClick = (e) => {
+    const characterId = e.target.closest('.character-card').id;
+    const currentCharacter = characters.find(x => {
+        return x.id === characterId
+});
+    console.log(currentCharacter);
+    
+}
+
+const createEvents = () => {
+    const characterCards = document.getElementsByClassName("character-card");
+    for(let i=0; i<characterCards.length; i++) {
+        characterCards[i].addEventListener('click', characterClick);
+    }
+}
 
 const charactersBuilder = () => {
     let domString = "";
     characters.forEach((character) => {
-        domString += `<div class= "col-2 character">`;
+        domString += `<div class= "col-2 character-card" id="${character.id}">`;
         domString += `<div class="card">`;
         domString += `<img class="card-img-top" src="${character.imageUrl}" alt="${character.name}">`
         domString += `<div class="card-body">`
@@ -23,6 +39,7 @@ const charactersBuilder = () => {
         domString += `</div>`;
     });
     printToDom(domString);
+    createEvents();
 }
 
 export {charactersBuilder};

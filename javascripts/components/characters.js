468070ead52eb7a486bdab9ce626226deb3fa242
Austin Cumberlander <austincumberlander@gmail.com>
2018-10-02 21:09:13 -0500

    wip

--- javascripts/components/characters.js
+++ javascripts/components/characters.js
@@ -9,6 +9,10 @@ const setCharacters = (newArray) => {
     characters = newArray;
 }
 
+const getCharacterz = () => {
+    return 
+}
+
 
 const characterClick = (e) => {
     const characterId = e.target.closest('.character-card').id;
@@ -24,9 +28,9 @@ const createEvents = () => {
     }
 }
 
-const charactersBuilder = () => {
+const charactersBuilder = (charactersArray) => {
     let domString = "";
-    characters.forEach((character) => {
+    charactersArray.forEach((character) => {
         domString += `<div class= "col-2 character-card" id="${character.id}">`;
         domString += `<div class="card">`;
         domString += `<img class="card-img-top" src="${character.imageUrl}" alt="${character.name}">`
